5421196fa8a688efec910ecd81ce3dd928292f2c
pduykhanh <pduykhanh@vnpt.vn>
2025-10-24 16:50:06 +0700

    update api URL

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class AuthService {
   //private apiUrl = 'http://localhost:3000/api/auth';
-  private apiUrl = 'http://chat-backend:3000/api/auth';
+  private apiUrl = 'http://103.77.242.45:3000/api/auth';
   private tokenKey = 'auth_token';
 
   constructor(private http: HttpClient, private router: Router) {}

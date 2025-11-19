a7c8312e0973e87e32e4b017f22f5cc20b413e65
pduykhanh <pduykhanh@vnpt.vn>
2025-11-19 16:15:40 +0700

    sua quy chuan cho datasource docker file va jenkins file

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,15 +1,15 @@
 import { DataSource } from 'typeorm';
 import { entities } from './database/entities';
 
-export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: process.env.DB_HOST,
-    port: 3306,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    entities,
-    migrations: ["dist/migrations/*.js"],
+export const dataSource = new DataSource({
+  type: 'mysql',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities,
+  migrations: ["dist/migrations/*.js"],
     synchronize: false,      // ❗ BẮT BUỘC
     migrationsRun: false,    // ❗ TRÁNH lỗi trùng bảng
     logging: false,

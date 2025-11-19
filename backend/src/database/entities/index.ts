5d015737503277d63e58a63980d2c72c11f3effa
pduykhanh <pduykhanh@vnpt.vn>
2025-11-19 16:00:47 +0700

    sua quy chuan cho datasource docker file va jenkins file

--- backend/src/database/entities/index.ts
+++ backend/src/database/entities/index.ts
@@ -1,10 +1,11 @@
 
 // ... import tất cả entity khác
 
+import { ChatRoom } from "./chat-room.entity";
 import { MessageAttachment } from "./message-attachment.entity";
 import { Message } from "./message.entity";
 import { Permission } from "./permission.entity";
 import { Role } from "./role.entity";
 import { User } from "./user.entity";
 
-export const entities = [User,Role,Permission,MessageAttachment,Message];
+export const entities = [User,Role,Permission,MessageAttachment,Message,ChatRoom];
